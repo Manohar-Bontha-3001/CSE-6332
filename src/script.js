cf344846f64b652b6af14c40901d8b20c3359150
Manohar-Bontha-3001 <141906934+Manohar-Bontha-3001@users.noreply.github.com>
2024-06-05 17:20:09 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
 
+window.onload = function() {
+  // Assuming you have retrieved user information from Azure Database
+  var userData = {
+    name: "Manohar Bontha",
+    id: "1002091309"
+  };
+
+  // Update the user information in the header
+  document.getElementById("username").textContent = userData.name;
+  document.getElementById("userid").textContent = "ID no " + userData.id;
+};
