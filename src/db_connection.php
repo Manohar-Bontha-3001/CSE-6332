fd15c61491289b8e30ad9f730b57e1bc2cfcd762
Manohar-Bontha-3001 <mxb1309@mavs.uta.edu>
2024-06-06 17:14:01 -0500

    new change

--- src/db_connection.php
+++ src/db_connection.php
@@ -1,13 +1,18 @@
 <?php
-$serverName = "manoharbontha.database.windows.net";
-$database = "Manoharbontha";
-$username = "manoharb";
-$password = "Santimanu1*";
+require 'vendor/autoload.php';
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=manoharbontha3001;AccountKey=lxe8KMUdlOG7NpjW/8OMqDlNdcGmxFRl6R5Ta/25R0H1lUgLrt6zSwpaAarKFc2Rt8wECtaV/yQN+ASt+UyWzg==;EndpointSuffix=core.windows.net";
+$containerName = "quiz1";
+$blobName = "q0c.csv";
 
 try {
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    // Create blob client.
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
+} catch (Exception $e) {
+    echo "Error connecting to Blob Storage: " . $e->getMessage();
+    exit;
 }
 ?>
